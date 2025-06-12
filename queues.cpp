7cfaa27db3e3a60ea314cb5a820892556b16e87f
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-12 10:14:04 +0700

    mengecek apakah antrian penuh atau kosong

--- queues.cpp
+++ queues.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class queues {
-    int front, rear, max =5;
+class queues
+{
+    int front, rear, max = 5;
     int queue_array[5];
 
-    public:
-    queues (){
-        front =-1;
-        rear =-1;
+public:
+    queues()
+    {
+        front = -1;
+        rear = -1;
     }
 
-    void insert (){
+    void insert()
+    {
         int num;
-        cout << "enter a number: " ;
+        cout << "enter a number: ";
         cin >> num;
         cout << endl;
+        // cek apakah antrian penuh
+
+        if ((front == 0 && rear == max - 1) || (front == rear + 1))
+        {
+            cout << "\nQueue overFlow\n";
+            return;
+        }
+        // cek apakah antrian kosong
+        if (front == -1)
+        {
+            front = 0;
+            rear = 0;
+        }
+       
     }
 };
